46fb4dbd3373fe4522c0a9092a2cf849334c1364
Tristan Bennett <tmbennett2011@msn.com>
2018-01-19 08:14:21 -0700

    added index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Raj is sick and tired of Leonard and Sheldon arguing
+ * He decides to model the two friends' characteristics:
+ * 	name: string
+ * 	stubborness: float
+ *
+ * write a short class with these state variables
+ * then start a method, argue(), that will echo "I AM RITE" when called
+ */
+
